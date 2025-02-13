c07a417cb810dd2b93753693d1bc161efb09cc67
albertrml <albertrml.dev@gmail.com>
2025-02-12 21:28:08 -0300

    Tela de Login operacional

--- android/Orgs/app/src/main/java/br/com/alura/orgs/view/adapter/Adapters.kt
+++ android/Orgs/app/src/main/java/br/com/alura/orgs/view/adapter/Adapters.kt
@@ -3,10 +3,7 @@ package br.com.alura.orgs.view.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import br.com.alura.orgs.R
-import br.com.alura.orgs.utils.exception.AccountException
-import br.com.alura.orgs.utils.tools.isPasswordValid
 import coil.load
-import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String) {
@@ -16,13 +13,3 @@ fun loadImage(view: ImageView, url: String) {
         view.load(R.drawable.ic_image_not_found)
 }
 
-@BindingAdapter("android:password")
-fun loadPassword(view: TextInputLayout, password: String) {
-    if (!password.isPasswordValid()) {
-        view.isErrorEnabled
-        view.error = AccountException.InvalidPassword().message
-    }
-    else {
-        view.isErrorEnabled = false
-    }
-}
