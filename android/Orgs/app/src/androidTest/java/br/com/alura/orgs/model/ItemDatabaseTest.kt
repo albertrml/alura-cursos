bc424cabfa41bbc03d3c52fc26a36af1eac48dee
arml <albertrml.dev@gmail.com>
2025-02-18 08:42:46 -0300

    ItemDatabaseTest ajustado

--- android/Orgs/app/src/androidTest/java/br/com/alura/orgs/model/ItemDatabaseTest.kt
+++ android/Orgs/app/src/androidTest/java/br/com/alura/orgs/model/ItemDatabaseTest.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertThrows
@@ -25,6 +26,8 @@ class ItemDatabaseTest {
     private lateinit var accountDao: AccountDAO
     private lateinit var itemDAO: ItemDAO
     private lateinit var db: OrgRoomDatabase
+    private val expectedItem = mockItems
+    private val nonExistentUserOwner = "TESTETESTE"
 
     @Before
     fun createDb(){
@@ -38,14 +41,16 @@ class ItemDatabaseTest {
     }
 
     @Before
-    fun setupTestData() = runTest {  db.clearAllTables() }
+    fun setupTestData() = runTest {
+        db.clearAllTables()
+        mockAccounts.forEach{ accountDao.insert(it) }
+    }
 
     @After
     fun closeDb(){ db.close() }
 
     @Test
     fun whenInsertItemIsSuccessful() = runTest {
-        accountDao.insert(mockAccounts[0])
         itemDAO.insert(mockItems[0])
 
         val itemFromDatabase = itemDAO.getItemById(1)
@@ -56,15 +61,49 @@ class ItemDatabaseTest {
     }
 
     @Test
+    fun whenGetItemByIdFindItem() = runTest {
+        itemDAO.insert(expectedItem[0])
+        val item = itemDAO.getItemById(1)
+        assertNotNull(item)
+    }
+
+    @Test
     fun whenGetItemByIdDoesNotFindItem() = runTest {
         val item = itemDAO.getItemById(1)
         assertNull(item)
     }
 
     @Test
+    fun whenGetItemsRetrievesProperlyAllItems() = runTest {
+        expectedItem.forEach { itemDAO.insert(it) }
+        val allItems = itemDAO.getItems().first().map { it.copy(id = 0) }
+        assert(allItems.size == expectedItem.size)
+        assert(allItems.containsAll(expectedItem))
+    }
+
+    @Test
+    fun whenGetItemsByUserOwnerRetrievesProperlyAllItems() = runTest {
+        val expectedRichardItems = expectedItem.filter { it.userOwner == "richard" }
+        expectedItem.forEach { itemDAO.insert(it) }
+        val richardItems = itemDAO
+            .getItemsByUserOwner("richard")
+            .first().map { it.copy(id = 0) }
+        assertEquals(expectedRichardItems.size, richardItems.size)
+        assert(richardItems.containsAll(expectedRichardItems))
+    }
+
+    @Test
+    fun whenGetItemsByWrongUserOwnerRetrievesEmpty() = runTest {
+        expectedItem.forEach { itemDAO.insert(it) }
+        val items = itemDAO
+            .getItemsByUserOwner(nonExistentUserOwner)
+            .first().map { it.copy(id = 0) }
+        assert(items.isEmpty())
+    }
+
+    @Test
     fun whenInsertDuplicateItemThrowsException() = runTest {
-        accountDao.insert(mockAccounts[0])
-        itemDAO.insert(mockItems[0])
+        itemDAO.insert(expectedItem[0])
         val item = itemDAO.getItemById(1)!!
         assertThrows(SQLiteConstraintException::class.java) {
             runBlocking { itemDAO.insert(item) }
@@ -73,38 +112,28 @@ class ItemDatabaseTest {
 
     @Test
     fun whenUpdateItemUpdatesSuccessfully() = runTest {
-        accountDao.insert(mockAccounts[2])
-        itemDAO.insert(mockItems[2])
+        itemDAO.insert(expectedItem[2])
         val itemBeforeUpdate = itemDAO.getItemById(1)!!.copy(
-            itemName = mockItems[1].itemName,
-            itemDescription = mockItems[1].itemDescription,
-            itemValue = mockItems[1].itemValue,
-            quantityInStock = mockItems[1].quantityInStock
+            itemName = expectedItem[1].itemName,
+            itemDescription = expectedItem[1].itemDescription,
+            itemValue = expectedItem[1].itemValue,
+            quantityInStock = expectedItem[1].quantityInStock
         )
         itemDAO.update(itemBeforeUpdate)
         val itemAfterUpdate = itemDAO.getItemById(1)
 
         assert(itemDAO.getItems().first().size == 1)
         assertEquals(itemBeforeUpdate, itemAfterUpdate)
+        assertNotEquals(expectedItem[2], itemAfterUpdate!!.copy(id = 0))
     }
 
     @Test
     fun whenDeleteItemRemovesSuccessfully() = runTest {
-        accountDao.insert(mockAccounts[0])
-        itemDAO.insert(mockItems[0])
+        itemDAO.insert(expectedItem[0])
         val itemForDelete = itemDAO.getItemById(1)!!
         assertEquals(1, itemDAO.getItems().first().size)
         itemDAO.delete(itemForDelete)
         assert(itemDAO.getItems().first().isEmpty())
-    }
-
-    @Test
-    fun whenGetItemsRetrievesProperlyAllItems() = runTest {
-        mockAccounts.forEach{ accountDao.insert(it) }
-        mockItems.forEach { itemDAO.insert(it) }
-        val allItems = itemDAO.getItems().first().map { it.copy(id = 0) }
-        assert(allItems.size == mockItems.size)
-        assert(allItems.containsAll(mockItems))
     }
 
 }
--- android/Orgs/app/src/main/java/br/com/alura/orgs/model/mock/ItemMock.kt
+++ android/Orgs/app/src/main/java/br/com/alura/orgs/model/mock/ItemMock.kt
@@ -2,6 +2,41 @@ package br.com.alura.orgs.model.mock
 
 import br.com.alura.orgs.model.entity.Item
 
+private val organics = mapOf(
+    "Fruit" to listOf("Orange", "Apple", "Banana", "Grape", "Strawberry", "Pineapple"),
+    "Vegetable" to listOf("Carrot", "Broccoli", "Potato", "Tomato", "Onion",
+                          "Lettuce", "Cucumber", "Spinach", "Cabbage", "Pepper",
+                          "Eggplant", "Zucchini", "Garlic", "Ginger")
+)
+
+private val usernames = mockAccounts.map { it.username }
+
+private fun randomUsername() = usernames.random()
+
+private fun randomOrganics(): Map<String,String> {
+    val description = organics.keys.random()
+    val name = organics[description]!!.random()
+    return mapOf(name to description)
+}
+
+private fun randomValue() = (10..100).random().toDouble() / 10
+
+private fun randomQuantity() = (10..100).random()
+
+val mockItems: List<Item> = List(20) { _ ->
+    val organic = randomOrganics()
+    val userOwner = randomUsername()
+    Item(
+        itemName = organic.keys.first(),
+        itemDescription = organic.values.first(),
+        itemValue = randomValue(),
+        quantityInStock = randomQuantity(),
+        itemUrl = "",
+        userOwner = userOwner
+    )
+}
+
+/*
 val mockItems = listOf(
     Item(
         itemName = "Orange",
@@ -43,4 +78,4 @@ val mockItems = listOf(
         itemUrl = "",
         userOwner = "pedro"
     )
-)
+)*/
