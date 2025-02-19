3d8225b6e99deb1908b49f07a282eea38d0b8410
albertrml <albertrml.dev@gmail.com>
2025-02-19 13:05:36 -0300

    Ajustes nos testes para AccountDAO

--- android/Orgs/app/src/androidTest/java/br/com/alura/orgs/model/AccountDatabaseTest.kt
+++ android/Orgs/app/src/androidTest/java/br/com/alura/orgs/model/AccountDatabaseTest.kt
@@ -1,27 +1,38 @@
 package br.com.alura.orgs.model
 
 import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import br.com.alura.orgs.model.entity.Account
 import br.com.alura.orgs.model.mock.mockAccounts
+import br.com.alura.orgs.model.mock.mockItems
 import br.com.alura.orgs.model.source.AccountDAO
+import br.com.alura.orgs.model.source.ItemDAO
 import br.com.alura.orgs.model.source.OrgRoomDatabase
+import br.com.alura.orgs.utils.tools.collectUntil
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertThrows
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
 class AccountDatabaseTest {
 
+    private lateinit var itemDao: ItemDAO
     private lateinit var dao: AccountDAO
     private lateinit var db: OrgRoomDatabase
     private val wrongUsername = "Test"
     private val wrongPassword = "Test123"
     private val newPassword = "65432A"
+    private val expectedAccounts = mockAccounts
+
 
     @Before
     fun setup(){
@@ -31,6 +42,7 @@ class AccountDatabaseTest {
             klass = OrgRoomDatabase::class.java
         ).build()
         dao = db.accountDao()
+        itemDao = db.itemDao()
     }
 
     @Before
@@ -39,70 +51,146 @@ class AccountDatabaseTest {
     @After
     fun closeDb(){ db.close() }
 
+    /*** Insert and Check If Account Exists ***/
+
     @Test
-    fun whenInsertAccountAndRetrieveAllUsernameIsSuccessful() = runTest {
-        mockAccounts.forEach { dao.insert(it) }
-        val accountsFromDatabase = dao.getAllUsernames()
-        assert(accountsFromDatabase.size == mockAccounts.size)
-        accountsFromDatabase.forEach { account ->
-            assert(mockAccounts.map { it.username }.contains(account))
+    fun whenInsertAndCheckIfAccountExists() = runTest {
+        val expectedAccount = expectedAccounts.first
+        dao.insert(expectedAccount)
+        assertTrue(
+            dao.isUsernameExist(expectedAccount.username)
+        )
+        assertFalse(
+            dao.isUsernameExist(wrongUsername)
+        )
+    }
+
+    @Test
+    fun whenInsertDuplicateAccount() = runTest {
+        val expectedAccount = expectedAccounts.first
+        val duplicateAccount = expectedAccount.copy()
+        dao.insert(expectedAccount)
+        assertThrows(SQLiteConstraintException::class.java){
+            runBlocking { dao.insert(duplicateAccount) }
         }
     }
 
+    /*** Authentication Account ***/
     @Test
-    fun whenSearchForUsernameAndDoesExist() = runTest{
-        mockAccounts.forEach { dao.insert(it) }
-        assert(dao.isUsernameExist(mockAccounts[0].username))
+    fun whenAuthenticateWithCorrectCredentials() = runTest {
+        val expectedAccount = expectedAccounts.first
+        dao.insert(expectedAccount)
+
+        val authenticatedAccount = expectedAccount?.run {
+            dao.authenticate(username, password)
+        } ?: assert(false)
+
+        assertEquals(expectedAccount, authenticatedAccount)
     }
 
     @Test
-    fun whenSearchForUsernameAndDoesNotExist() = runTest{
-        mockAccounts.forEach { dao.insert(it) }
-        assert(!dao.isUsernameExist(wrongUsername))
+    fun whenAuthenticateWithWrongCredentials() = runTest {
+        val expectedAccount = expectedAccounts.first
+        dao.insert(expectedAccount)
+
+        assertNull(
+            dao.authenticate(wrongUsername, wrongPassword)
+        )
+
+        assertNull(
+            dao.authenticate(expectedAccount.username, wrongPassword)
+        )
+
+        assertNull(
+            dao.authenticate(wrongUsername, expectedAccount.password)
+        )
+    }
+
+    /*** Get All Usernames ***/
+    @Test
+    fun whenGetAllUsernamesIsSuccessful() = runTest {
+        expectedAccounts.forEach { dao.insert(it) }
+        val usernames = dao.getAllUsernames()
+        assertEquals(usernames.size, expectedAccounts.size)
+        assert(
+            usernames.containsAll(expectedAccounts.map { it.username })
+        )
+    }
+
+    /*** Update Account ***/
+    @Test
+    fun whenUpdateAccountARegisteredAccountIsSuccessful() = runTest{
+        val account = expectedAccounts.first
+        val updatedAccount = account.copy(password = newPassword)
+        dao.insert(account)
+        dao.update(updatedAccount)
+        val accountFromDatabase = updatedAccount.run {
+            dao.authenticate(username, password)
+        }
+        assertEquals(updatedAccount, accountFromDatabase)
+        assertNotEquals(account, accountFromDatabase)
     }
 
     @Test
-    fun whenAuthenticateWithCorrectPassword() = runTest{
-        mockAccounts.forEach { dao.insert(it) }
-        val authenticatedAccount = dao
-            .authenticate(mockAccounts[0].username, mockAccounts[0].password)
-        assertEquals(authenticatedAccount, mockAccounts[0])
+    fun whenUpdateAccountARegisteredAccountWithTheSameCredentialsIsSuccessful() = runTest{
+        val expectedAccount = expectedAccounts.first
+        dao.insert(expectedAccount)
+        dao.update(expectedAccount)
+        val accountFromDatabase = expectedAccount.run {
+            dao.authenticate(username, password)
+        }
+        assertEquals(expectedAccount, accountFromDatabase)
     }
 
     @Test
-    fun whenAuthenticateWithWrongPassword() = runTest{
-        mockAccounts.forEach { dao.insert(it) }
-        val authenticatedAccount = dao.authenticate(mockAccounts[0].username, wrongPassword)
-        assertNull(authenticatedAccount)
-    }
+    fun whenUpdateAccountAUnregisteredAccountThrowsException() = runTest {
+        val unregisteredAccount = expectedAccounts.first
+        dao.update(unregisteredAccount)
 
-    @Test
-    fun whenUpdateAccountIsSuccessful() = runTest{
-        mockAccounts.forEach { dao.insert(it) }
-        val accountForUpdate = mockAccounts[0].copy(password = newPassword)
-        dao.update(accountForUpdate)
-        with(accountForUpdate){
-            val updatedAccount = dao.authenticate(username, newPassword)
-            assertEquals(updatedAccount, accountForUpdate)
-            assertNotEquals(updatedAccount, mockAccounts[0])
+        unregisteredAccount.run {
+            assertNull(dao.authenticate(username, password))
+            assertFalse(dao.isUsernameExist(username))
+        }
+
+        dao.insert(unregisteredAccount)
+        val otherUnregisteredAccount = unregisteredAccount.copy(username = wrongUsername)
+
+        otherUnregisteredAccount.let {
+            dao.update(it)
+            assertNull(dao.authenticate(it.username, it.password))
+            assertFalse(dao.isUsernameExist(it.username))
         }
     }
 
+    /*** Delete Account ***/
     @Test
     fun whenDeleteAccountIsSuccessful() = runTest {
-        mockAccounts.forEach { dao.insert(it) }
-        val accountForDelete = mockAccounts[0]
-        assert(dao.isUsernameExist(accountForDelete.username))
-        dao.delete(accountForDelete)
-        assertNull(dao.authenticate(accountForDelete.username, accountForDelete.password))
-    }
+        val expectedAccount = expectedAccounts.first
+        dao.insert(expectedAccount)
 
-    @Test
-    fun whenDeleteAccountIsUnsuccessful() = runTest {
-        mockAccounts.forEach { dao.insert(it) }
-        val accountForDelete = Account(username = wrongUsername, password = wrongPassword)
-        dao.delete(accountForDelete)
-        assertEquals(dao.getAllUsernames().size, mockAccounts.size)
+        val itemsToInsert = mockItems.filter { it.userOwner == expectedAccount.username }
+        itemsToInsert.forEach { itemDao.insert(it) }
+
+        itemDao.getItemsByUserOwner(expectedAccount.username)
+            .collectUntil { items -> items.size == itemsToInsert.size  }
+            .collectLatest { items ->
+                val itemsFromDatabase = items.map { it.copy(id = 0) }
+                assertEquals(itemsToInsert.size, items.size)
+                assert(itemsFromDatabase.containsAll(itemsToInsert))
+            }
+
+        dao.delete(expectedAccount)
+
+        expectedAccount.run {
+            assertNull(dao.authenticate(username, password))
+            assertFalse(dao.isUsernameExist(username))
+
+            itemDao.getItemsByUserOwner(username)
+                .collectUntil { items -> items.isEmpty()  }
+                .collectLatest { items ->
+                    assertTrue(items.isEmpty())
+                }
+        }
     }
 
 }
--- android/Orgs/app/src/main/java/br/com/alura/orgs/model/source/AccountDAO.kt
+++ android/Orgs/app/src/main/java/br/com/alura/orgs/model/source/AccountDAO.kt
@@ -11,22 +11,22 @@ import br.com.alura.orgs.model.entity.Account
 @Dao
 interface AccountDAO {
 
+    @Query("SELECT * FROM account WHERE username = :username AND password = :password")
+    suspend fun authenticate(username: String, password: String): Account?
+
+    @Delete
+    suspend fun delete(account: Account)
+
     @Insert (onConflict = OnConflictStrategy.ABORT)
     suspend fun insert(account: Account)
+
+    @Query("SELECT EXISTS (SELECT * FROM account WHERE username = :username)")
+    suspend fun isUsernameExist(username: String): Boolean
 
     @Query("SELECT username FROM account")
     suspend fun getAllUsernames(): List<String>
 
     @Update
     suspend fun update(account: Account)
-
-    @Delete
-    suspend fun delete(account: Account)
-
-    @Query("SELECT * FROM account WHERE username = :username AND password = :password")
-    suspend fun authenticate(username: String, password: String): Account?
-
-    @Query("SELECT EXISTS (SELECT * FROM account WHERE username = :username)")
-    suspend fun isUsernameExist(username: String): Boolean
 
 }
