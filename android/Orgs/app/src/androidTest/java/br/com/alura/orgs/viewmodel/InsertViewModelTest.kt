cc306d9f6734a8bfc0bf4698c0e38444d45d826d
arml <albertrml.dev@gmail.com>
2025-02-22 12:42:55 -0300

    Ajustes em InsertViewModelTest

--- android/Orgs/app/src/androidTest/java/br/com/alura/orgs/viewmodel/InsertViewModelTest.kt
+++ android/Orgs/app/src/androidTest/java/br/com/alura/orgs/viewmodel/InsertViewModelTest.kt
@@ -2,31 +2,38 @@ package br.com.alura.orgs.viewmodel
 
 import android.content.Context
 import android.database.sqlite.SQLiteConstraintException
+import android.util.Log
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import br.com.alura.orgs.domain.InsertUseCase
 import br.com.alura.orgs.model.entity.ItemUi
+import br.com.alura.orgs.model.mock.mockAccounts
 import br.com.alura.orgs.model.mock.mockItems
 import br.com.alura.orgs.model.repository.AccountRepository
 import br.com.alura.orgs.model.repository.ItemRepository
 import br.com.alura.orgs.model.source.AccountDAO
 import br.com.alura.orgs.model.source.ItemDAO
 import br.com.alura.orgs.model.source.OrgRoomDatabase
+import br.com.alura.orgs.utils.data.Authenticate
 import br.com.alura.orgs.utils.data.Response.*
 import br.com.alura.orgs.utils.tools.until
 import br.com.alura.orgs.viewmodel.insert.InsertUiEvent
 import br.com.alura.orgs.viewmodel.insert.InsertViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
 
 class InsertViewModelTest {
-
 
     private lateinit var viewModel: InsertViewModel
     private lateinit var insertUseCase: InsertUseCase
@@ -61,43 +68,78 @@ class InsertViewModelTest {
 
     @Test
     fun whenOnInsertItemIsSuccessful() = runTest {
-        val itemUiMock = ItemUi.fromItem(mockItems[0])
+        val account = mockAccounts.first
+        val expectedItem = mockItems.first{ it.userOwner == account.username }
+        val itemUiToInsert = ItemUi.fromItem(expectedItem)
 
-        viewModel.onEvent(InsertUiEvent.OnInsert(itemUiMock))
+        accountDAO.insert(account)
+
+        combine(
+            accountRepository.authenticate(account.username, account.password),
+            accountRepository.auth
+        ) { _, auth ->
+            if (auth is Authenticate.Login){
+                viewModel.onEvent(InsertUiEvent.OnInsert(itemUiToInsert))
+            }
+            auth
+        }
+            .until { it is Authenticate.Login }
+            .collect()
+
         viewModel.uiState
-            .until { uiState -> uiState.insertState is Success }
-            .collect { uiState ->
-                when (uiState.insertState) {
+            .map { uiState -> uiState.insertState }
+            .until { insertState -> insertState is Success }
+            .collect{ insertState ->
+                when(insertState){
                     is Success -> {
-                        val itemFromViewModel = itemDAO.getItemById(1)!!.copy(
+                        val itemFromDB = itemDAO.getItemById(1)!!.copy(
                             id = mockItems[0].id
                         )
-                        assertEquals(mockItems[0], itemFromViewModel)
+                        assertEquals(expectedItem, itemFromDB)
                     }
-
-                    is Loading -> assert(true)
-                    is Failure -> assert(false)
+                    is Loading -> {
+                        assertTrue(true)
+                    }
+                    is Failure -> {
+                        assertFalse(true)
+                    }
                 }
             }
+
     }
 
     @Test
     fun whenInsertDuplicateItemButDifferentIdsIsSuccessful() = runTest {
+        val account = mockAccounts.first
+        val item = mockItems.first{ it.userOwner == account.username }
+        val itemUiMock = ItemUi.fromItem(item)
 
-        val itemUiMock = ItemUi.fromItem(mockItems[0])
-        viewModel.onEvent(InsertUiEvent.OnInsert(itemUiMock))
-        viewModel.onEvent(InsertUiEvent.OnInsert(itemUiMock))
+        accountDAO.insert(account)
+
+        combine(
+            accountRepository.authenticate(account.username, account.password),
+            accountRepository.auth
+        ) { _, auth ->
+            if (auth is Authenticate.Login){
+                viewModel.onEvent(InsertUiEvent.OnInsert(itemUiMock))
+                viewModel.onEvent(InsertUiEvent.OnInsert(itemUiMock))
+            }
+            auth
+        }
+            .until { it is Authenticate.Login }
+            .collect()
+
 
         viewModel.uiState
-            .until { uiState -> uiState.insertState is Success }
-            .collect { uiState ->
-                when (uiState.insertState) {
+            .map { uiState -> uiState.insertState }
+            .until { insertState -> insertState is Success }
+            .collect { insertState ->
+                when (insertState) {
                     is Success -> {
                         val itemFromViewModel = itemDAO.getItems().first()
                         assertEquals(itemFromViewModel.size, 2)
                         assertNotEquals(itemFromViewModel[0], itemFromViewModel[1])
                     }
-
                     is Loading -> assert(true)
                     is Failure -> assert(false)
                 }
@@ -106,9 +148,25 @@ class InsertViewModelTest {
 
     @Test
     fun whenInsertDuplicateItemIsUnsuccessful() = runTest {
-        itemDAO.insert(mockItems.first())
+        val account = mockAccounts.first
+        val item = mockItems.first{ it.userOwner == account.username }
+        accountDAO.insert(account)
+        itemDAO.insert(item)
+
         val itemFromDatabase = ItemUi.fromItem(itemDAO.getItemById(1)!!)
-        viewModel.onEvent(InsertUiEvent.OnInsert(itemFromDatabase))
+
+        combine(
+            accountRepository.authenticate(account.username, account.password),
+            accountRepository.auth
+        ) { _, auth ->
+            if (auth is Authenticate.Login){
+                viewModel.onEvent(InsertUiEvent.OnInsert(itemFromDatabase))
+            }
+            auth
+        }
+            .until { it is Authenticate.Login }
+            .collect()
+
         viewModel.uiState
             .until { uiState -> uiState.insertState is Failure }
             .collect { uiState ->
